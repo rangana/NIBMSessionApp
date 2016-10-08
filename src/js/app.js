66464ac27a6956cee4805519ccf0405e69141f4c
Rangana Sampath <rangana.sampath@pearson.com>
2016-10-09 01:53:50 +0530

    adding new changes to the app

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,19 @@
 (function () {
-    /*
-     declare the main application
+    /**
+     * declare the main application
      */
-    var app = angular.module('app', []);
+    var app = angular.module('app', ['ui.bootstrap']);
 
-    /*
-     main controller declaration
+    /**
+     * main controller declaration
      */
-    app.controller('mainController', ["$scope", "dataConsumerService", function ($scope, dataConsumerService) {
+    app.controller('mainController', ["$scope", "dataConsumerService", "$uibModal", function ($scope, dataConsumerService, $uibModal) {
         $scope.notes = dataConsumerService.getNotes();
-
         $scope.addNewNote = function () {
-            alert("Add New Note");
+            $scope.modalInstance = $uibModal.open({
+                templateUrl: 'myTestModal.tmpl.html',
+                scope: $scope
+            });
         };
 
         $scope.editNote = function (noteData) {
@@ -24,10 +26,59 @@
             console.log(noteData);
         };
 
+
+        /* modal window functions */
+
+        $scope.note = {
+            title: "",
+            desc: ""
+        };
+
+        $scope.close = function () {
+            $scope.modalInstance.dismiss();//$scope.modalInstance.close() also works I think
+        };
+
+        $scope.saveNote = function () {
+
+            var today = new Date();
+            var dd = today.getDate();
+            var mm = today.getMonth() + 1; //January is 0!
+            var yyyy = today.getFullYear();
+            if (dd < 10) {
+                dd = '0' + dd
+            }
+            if (mm < 10) {
+                mm = '0' + mm
+            }
+            var date = mm + '/' + dd + '/' + yyyy;
+
+
+            var hours = today.getHours();
+            var minutes = today.getMinutes();
+            var ampm = (hours >= 12 ) ? 'pm' : 'am';
+            hours = hours % 12;
+            hours = hours ? hours : 12; // the hour '0' should be '12'
+            minutes = minutes < 10 ? '0' + minutes : minutes;
+            var time = hours + ':' + minutes + ' ' + ampm;
+
+            /**
+             * call the save API , data is not validated
+             */
+            dataConsumerService.addNotes({
+                topic: $scope.note.title,
+                description: $scope.note.desc,
+                time: time,
+                date: date
+            });
+
+            $scope.modalInstance.dismiss();
+
+        };
+
     }]);
 
-    /*
-     http service consuming service declaration
+    /**
+     * http service consuming service declaration
      */
     app.service('dataConsumerService', function () {
 
@@ -35,58 +86,58 @@
             {
                 topic: "sample topic one",
                 description: "sample description one sample description one sample description one",
-                time: "01.00PM",
-                date: "2016 Sep 01"
+                time: "7:35 pm",
+                date: "10/08/2016"
             }, {
                 topic: "sample topic one",
                 description: "sample description one sample description one sample description one",
-                time: "01.00PM",
-                date: "2016 Sep 01"
+                time: "7:35 pm",
+                date: "10/08/2016"
             }, {
                 topic: "sample topic one",
                 description: "sample description one sample description one",
-                time: "01.00PM",
-                date: "2016 Sep 01"
+                time: "7:35 pm",
+                date: "10/08/2016"
             }, {
                 topic: "sample topic one",
                 description: "sample description one",
-                time: "01.00PM",
-                date: "2016 Sep 01"
+                time: "7:35 pm",
+                date: "10/08/2016"
             }, {
                 topic: "sample topic one",
                 description: "sample description one sample description one sample description onesample description one sample description onesample description one sample description onesample description one sample description one",
-                time: "01.00PM",
-                date: "2016 Sep 01"
+                time: "7:35 pm",
+                date: "10/08/2016"
             }, {
                 topic: "sample topic one",
                 description: "sample description one",
-                time: "01.00PM",
-                date: "2016 Sep 01"
+                time: "7:35 pm",
+                date: "10/08/2016"
             }, {
                 topic: "sample topic one",
                 description: "sample description one sample description one sample description one sample description one sample description one",
-                time: "01.00PM",
-                date: "2016 Sep 01"
+                time: "7:35 pm",
+                date: "10/08/2016"
             }, {
                 topic: "sample topic one",
                 description: "sample description one sample description one sample description one",
-                time: "01.00PM",
-                date: "2016 Sep 01"
+                time: "7:35 pm",
+                date: "10/08/2016"
             }, {
                 topic: "sample topic one",
                 description: "sample description one sample description one ",
-                time: "01.00PM",
-                date: "2016 Sep 01"
+                time: "7:35 pm",
+                date: "10/08/2016"
             }, {
                 topic: "sample topic one",
                 description: "sample description one sample description one sample description one",
-                time: "01.00PM",
-                date: "2016 Sep 01"
+                time: "7:35 pm",
+                date: "10/08/2016"
             }, {
                 topic: "sample topic one",
                 description: "sample description one sample description one",
-                time: "01.00PM",
-                date: "2016 Sep 01"
+                time: "7:35 pm",
+                date: "10/08/2016"
             }
         ];
 
@@ -101,14 +152,14 @@
         };
 
         dataConsumerService.deleteNote = function (noteId) {
-            /*
-             logic to delete the note with given ID
+            /**
+             * logic to delete the note with given ID
              */
         };
 
         dataConsumerService.changeNote = function (noteId, newNote) {
-            /*
-             logic to change the note with given noteId
+            /**
+             * logic to change the note with given noteId
              */
         };
 
